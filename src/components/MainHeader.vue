a94239cf50d6876e8419a8ae81382e828388e282
Slava <slavapank94@yandex.ru>
2023-03-17 15:02:09 +0600

    feat: create notFound page, add normalize & some styles

---
+++ src/components/MainHeader.vue
@@ -0,0 +1,25 @@
+<template>
+  <header class="header">
+    <router-link class="header__logo logo" :to="{ name: 'home' }">
+      <picture>
+        <source srcset="../assets/logo.webp" type="image/webp">
+        <img src="../assets/logo.png"
+             loading="lazy"
+             class="image"
+             width="182"
+             height="27"
+             alt="Логотип">
+      </picture>
+    </router-link>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'MainHeader',
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="not-found">
+    <h1>
+      Страница не найдена
+    </h1>
+    <router-link :to="{ name: 'home' }">На главную</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NotFoundView',
+};
+</script>
+
+<style lang="scss">
+.not-found {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
