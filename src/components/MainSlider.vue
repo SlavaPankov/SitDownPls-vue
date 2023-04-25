6ef76d37fec112dd75ecec82c87e346b0617d7c0
Slava <slavapank94@yandex.ru>
2023-04-25 17:43:50 +0600

    feat: banner adaptive

--- src/components/MainSlider.vue
+++ src/components/MainSlider.vue
@@ -58,13 +58,22 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 @import '../assets/scss/_mixins.scss';
 
 .banner {
   &__container {
     padding-top: 80px;
     padding-bottom: 80px;
+
+    @include mobile {
+      padding-top: 55px;
+      padding-bottom: 0;
+    }
+
+    @include small-mobile {
+      padding-top: 30px;
+    }
   }
 
   &__heading {
@@ -73,10 +82,20 @@ export default {
     line-height: 65px;
     font-weight: 700;
     color: var(--grey);
+
+    @include mobile {
+      font-size: 6.25vw;
+      line-height: 120%;
+      margin-bottom: 43vw;
+    }
   }
 
   &__button {
     @include btn-primary;
+
+    @include mobile {
+      width: 100%;
+    }
   }
 
   &__slide {
@@ -85,22 +104,80 @@ export default {
     &:first-child {
       background: url("../assets/img/1-banner.webp") center center no-repeat var(--white);
       background-size: cover;
+
+      @include tablet {
+        background: url("@/assets/img/1-banner-1024.webp") center center no-repeat var(--white);
+      }
+
+      @include small-tablet {
+        background: url("@/assets/img/1-banner-768.webp") center center no-repeat var(--white);
+      }
+
+      @include mobile {
+        background: url("@/assets/img/1-banner-320.png") center center no-repeat var(--white);
+        background-size: contain;
+        background-position: center top;
+      }
     }
 
     &:nth-child(2) {
       background: url("../assets/img/2-banner.webp") center center no-repeat var(--white);
       background-size: cover;
+
+      @include tablet {
+        background: url("@/assets/img/2-banner-1024.webp") center center no-repeat var(--white);
+      }
+
+      @include small-tablet {
+        background: url("@/assets/img/2-banner-768.webp") center center no-repeat var(--white);
+      }
+
+      @include mobile {
+        background: url("@/assets/img/2-banner-320.png") center center no-repeat var(--white);
+        background-size: contain;
+        background-position: center top;
+      }
     }
 
     &:nth-child(3) {
       background: url("../assets/img/3-banner.webp") center center no-repeat var(--grey_shade);
       background-size: cover;
 
+      @include tablet {
+        background: url("@/assets/img/3-banner-1024.webp") center center no-repeat var(--white);
+      }
+
+      @include small-tablet {
+        background: url("@/assets/img/3-banner-768.webp") center center no-repeat var(--white);
+      }
+
+      @include mobile {
+        background: url("@/assets/img/3-banner-320.webp") center center no-repeat var(--white);
+        background-size: contain;
+        background-position: center top;
+      }
+
       & .banner__heading {
         color: var(--white);
       }
     }
   }
+}
+
+.swiper-horizontal>.swiper-pagination-bullets,
+.swiper-pagination-bullets.swiper-pagination-horizontal,
+.swiper-pagination-custom,
+.swiper-pagination-fraction {
+  bottom: var(--swiper-pagination-bottom, 23px);
+
+  @include mobile {
+    bottom: var(--swiper-pagination-bottom, 25%);
+  }
+}
+
+.swiper-horizontal>.swiper-pagination-bullets .swiper-pagination-bullet,
+.swiper-pagination-horizontal.swiper-pagination-bullets .swiper-pagination-bullet {
+  margin: 0 var(--swiper-pagination-bullet-horizontal-gap, 8px);
 }
 
 .block {
